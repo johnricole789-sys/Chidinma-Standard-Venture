8575e4ccfbbb7506d04d8cf0fcd7692bc2a794fc
Lionel <YOUR-GITHUB-EMAIL>
2026-08-25 23:35:33 +0100

    Connect production app to PostgreSQL

--- config.py
+++ config.py
@@ -5,7 +5,10 @@ class Config:
 
     BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 
-    SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(BASE_DIR, "instance", "database.db")
+    SQLALCHEMY_DATABASE_URI = os.environ.get(
+    "DATABASE_URL",
+    "sqlite:///" + os.path.join(BASE_DIR, "instance", "database.db")
+)
 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
